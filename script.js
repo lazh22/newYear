31dba2d13ddc5e79554489efb1505ca913ae1721
lazh22 <rapapap98@gmail.com>
2020-12-20 16:46:58 +0500

    years

--- script.js
+++ script.js
@@ -3,22 +3,24 @@ $(document).ready(function () {
     music = new Howl({
         src: ["audio/1.mp3", "audio/2.mp3", "audio/3.mp3"],
         loop: true,
-        volume: 0.5,
-    })
+        volume: 0.5
+    });
 });
-$("#tree>img").click(function(e){
+$("#tree>img").click(function(e){ 
     music.stop();
-    let name = e.target.id
-    if(name == "toy1"){   
-        music.play(0);
-    }
-    else if(name == "toy2"){
+    let name = e.target.id;
+    if(name == "toy1"){
         music.play(1);
     }
-    else if(name == "toy3"){
+    else if(name == "toy2"){
         music.play(2);
     }
+    else if(name == "toy3"){
+        music.play(3);
+    }
 });
+
+
 
 let timer = setInterval(function(){
     let newYear = new Date("1 January 2021 00:00:00");
@@ -30,9 +32,9 @@ let timer = setInterval(function(){
     let minutes = Math.floor( (diff%(60*60*1000)) / (60*1000));
     let seconds = Math.floor( (diff%(60*1000)) / 1000);
 
-
     $("#seconds").text(seconds + " секунд");
     $("#minutes").text(minutes + " минут");
     $("#hours").text(hours + " часов");
     $("#days").text(days + " дней");
+
 }, 1000)
