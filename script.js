201a6965b042797f04f5785cd25310b03ceae299
lazh22 <rapapap98@gmail.com>
2020-12-20 16:36:15 +0500

    year

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 let music;
 $(document).ready(function () {
     music = new Howl({
-        src: ["https:\/\/ruv.hotmo.org\/get\/cuts\/de\/c8\/dec8b6cc57dccaad9b33fee2d14115da\/67639067\/Midnight_Riders_-_All_I_Want_For_Christmas_b128f0d211.mp3"],
+        src: ["audio/1.mp3", "audio/2.mp3", "audio/3.mp3"],
         loop: true,
-        volume: 0.75
-
-    });
-    $("#player>img").click(function (e){
-        if(e.target.id = "isPlaying"){
-            music.play();
-            e.target.id = "isPlaying";
-        }
-        
-    
-    });
+        volume: 0.5,
+    })
 });
+$("#tree>img").click(function(e){
+    music.stop();
+    let name = e.target.id
+    if(name == "toy1"){   
+        music.play(0);
+    }
+    else if(name == "toy2"){
+        music.play(1);
+    }
+    else if(name == "toy3"){
+        music.play(2);
+    }
+});
+
+let timer = setInterval(function(){
+    let newYear = new Date("1 January 2021 00:00:00");
+    let now = new Date();
+    let diff = newYear - now;
+
+    let days = Math.floor( diff/(24*60*60*1000) );
+    let hours = Math.floor( (diff%(24*60*60*1000)) / (60*60*1000) );
+    let minutes = Math.floor( (diff%(60*60*1000)) / (60*1000));
+    let seconds = Math.floor( (diff%(60*1000)) / 1000);
+
+
+    $("#seconds").text(seconds + " секунд");
+    $("#minutes").text(minutes + " минут");
+    $("#hours").text(hours + " часов");
+    $("#days").text(days + " дней");
+}, 1000)
