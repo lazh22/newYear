eb2f84f48822f84b1dedc4e56f616495d5591b55
lazh22 <rapapap98@gmail.com>
2020-12-27 17:31:16 +0500

    HappyNewYear

--- script.js
+++ script.js
@@ -1,40 +1,107 @@
-let music;
-$(document).ready(function () {
-    music = new Howl({
-        src: ["audio/1.mp3", "audio/2.mp3", "audio/3.mp3"],
-        loop: true,
-        volume: 0.5
-    });
+// Список музыки
+let music1 = new Howl({
+    src: ["audio/1.mp3"],
+    loop: true,
+    volume: 0.5
 });
-$("#tree>img").click(function(e){ 
-    music.stop();
+let music2 = new Howl({
+    src: ["audio/2.mp3"],
+    loop: true,
+    volume: 0.5
+});
+let music3 = new Howl({
+    src: ["audio/3.mp3"],
+    loop: true,
+    volume: 0.5
+});
+// Проигрыш музыки при нажатии на игрушку (Компутер)
+$("#tree>img").click(function (e) {
+    $("#tree>img").removeClass("shaking")
     let name = e.target.id;
-    if(name == "toy1"){
-        music.play(1);
-    }
-    else if(name == "toy2"){
-        music.play(2);
-    }
-    else if(name == "toy3"){
-        music.play(3);
+
+    if (name == "toy1") {
+        $(e.target).addClass("shaking");
+        music2.stop();
+        music3.stop();
+
+        music1.play();
+    } else if (name == "toy2") {
+        $(e.target).addClass("shaking");
+        music1.stop();
+        music3.stop();
+
+        music2.play();
+    } else if (name == "toy3") {
+        $(e.target).addClass("shaking");
+        music2.stop();
+        music1.stop();
+
+        music3.play();
     }
 });
+// Остановка музыки при нажатии на звезду (Компутер)
+$("#tree>#treeStar").click(function (e) {
+    $("#tree>img").removeClass("shaking")
+    music1.stop();
+    music2.stop();
+    music3.stop();
+});
+// Проигрыш музыки при нажатии на игрушку (Смартфон)
+$("#treeMobile>img").click(function (e) {
+    $("#treeMobile>img").removeClass("shaking")
+    let name = e.target.id;
 
+    if (name == "toy1") {
+        $(e.target).addClass("shaking");
+        music2.stop();
+        music3.stop();
 
+        music1.play();
+    } else if (name == "toy2") {
+        $(e.target).addClass("shaking");
+        music1.stop();
+        music3.stop();
 
-let timer = setInterval(function(){
+        music2.play();
+    } else if (name == "toy3") {
+        $(e.target).addClass("shaking");
+        music2.stop();
+        music1.stop();
+
+        music3.play();
+    }
+});
+// Остановка музыки при нажатии на звезду (Смартфон)
+$("#treeMobile>#treeStar").click(function (e) {
+    $("#treeMobile>img").removeClass("shaking")
+    music1.stop();
+    music2.stop();
+    music3.stop();
+});
+
+let timer = setInterval(function () {
     let newYear = new Date("1 January 2021 00:00:00");
     let now = new Date();
     let diff = newYear - now;
+    let days = Math.floor(diff / (24 * 60 * 60 * 1000));
+    let hours = Math.floor((diff % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+    let minutes = Math.floor((diff % (60 * 60 * 1000)) / (60 * 1000));
+    let seconds = Math.floor((diff % (60 * 1000)) / 1000);
 
-    let days = Math.floor( diff/(24*60*60*1000) );
-    let hours = Math.floor( (diff%(24*60*60*1000)) / (60*60*1000) );
-    let minutes = Math.floor( (diff%(60*60*1000)) / (60*1000));
-    let seconds = Math.floor( (diff%(60*1000)) / 1000);
-
-    $("#seconds").text(seconds + " секунд");
-    $("#minutes").text(minutes + " минут");
-    $("#hours").text(hours + " часов");
-    $("#days").text(days + " дней");
-
+    $(".seconds").text(seconds + " секунд");
+    $(".minutes").text(minutes + " минут");
+    $(".hours").text(hours + " часов");
+    $(".days").text(days + " дней");
 }, 1000)
+$(".hat").click(function (e) {
+    $(e.currentTarget).addClass("hatBounce");
+    setTimeout(function () {
+        $(e.currentTarget).removeClass("hatBounce");
+    }, 1500)
+});
+setInterval(function () {
+    $(".hand-r").addClass("hand-rSwing");
+    setTimeout(function () {
+        $(".hand-r").removeClass("hand-rSwing");
+    }, 1500)
+}, 3500)
